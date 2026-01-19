23906bec4403a0c26f70d37873e9eea459383ffa
student <Midnight>
2026-01-19 20:51:16 +0200

    refactor upload

---
+++ ChoiceEditors.py
@@ -0,0 +1,8 @@
+import wx.grid
+
+
+GPRChoiceEditor = wx.grid.GridCellChoiceEditor(["G","P","R"])
+GPChoiceEditor = wx.grid.GridCellChoiceEditor(["G","P"])
+PRMGChoiceEditor = wx.grid.GridCellChoiceEditor(["P","R","R+Mg"])
+GPP2RChoiceEditor = wx.grid.GridCellChoiceEditor(["G","P","P2","R"])
+NullChoiceEditor = wx.grid.GridCellChoiceEditor(["N/A"])
